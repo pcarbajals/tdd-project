acf737db5a4bb2556a2be6068df1848df330de65
Pablo Carbajal <pcarbajals@gmail.com>
2022-04-30 22:03:48 -0400

    Shell script for running all tests at once

---
+++ run_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo ..........................................................................
+echo
+echo "Running Go tests"
+echo 
+export GO111MODULE="on"
+export GOPATH=""
+cd ./go
+go test -v .
+cd ..
+echo
+echo ..........................................................................
+echo
+echo "Running Javascript tests"
+echo 
+node js/test_money.js
+echo "OK (if no messages above)"
+echo
+echo ..........................................................................
+echo
+echo "Running Python tests"
+echo 
+python3 py/test_money.py -v
+echo ..........................................................................
+
