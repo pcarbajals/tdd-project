e8d2eb8801ba576a63e92f3bb3a8e5eef24481ab
Pablo Carbajal <pcarbajals@gmail.com>
2022-05-02 15:58:14 -0400

    Removes redundant test

--- js/test_money.js
+++ js/test_money.js
@@ -3,10 +3,6 @@ const Money = require('./money')
 const Portfolio = require('./portfolio')
 
 
-let fiveDollars = new Money(5, "USD");
-let tenDollars = new Money(10, "USD");
-assert.deepStrictEqual(fiveDollars.times(2), tenDollars)
-
 let tenEuros = new Money(10, "EUR");
 let twentyEuros = new Money(20, "EUR");
 assert.deepStrictEqual(tenEuros.times(2), twentyEuros)
@@ -14,6 +10,8 @@ assert.deepStrictEqual(tenEuros.times(2), twentyEuros)
 let money = new Money(4002, "KRW");
 assert.deepStrictEqual(money.divide(4), new Money(1000.5, "KRW"))
 
+let fiveDollars = new Money(5, "USD");
+let tenDollars = new Money(10, "USD");
 let portfolio = new Portfolio();
 portfolio.add(fiveDollars, tenDollars)
 assert.deepStrictEqual(portfolio.evaluate("USD"), new Money(15, "USD"))
