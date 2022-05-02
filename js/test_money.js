4e1ee9da211ce556c5351b8f6ccc3d13e0d00f38
Pablo Carbajal <pcarbajals@gmail.com>
2022-05-02 16:16:39 -0400

    Discovers and automatically runs all test cases

--- js/test_money.js
+++ js/test_money.js
@@ -23,10 +23,21 @@ class MoneyTest {
         assert.deepStrictEqual(portfolio.evaluate("USD"), new Money(15, "USD"))
     }
 
+    getAllTestMethods() {
+        let moneyPrototype = MoneyTest.prototype;
+        let allProps = Object.getOwnPropertyNames(moneyPrototype);
+        let testMethods = allProps.filter(p => {
+            return typeof moneyPrototype[p] === 'function' && p.startsWith("test");
+        });
+        return testMethods;
+    }
+
     runAllTest() {
-        this.testMultiplication();
-        this.testDivision();
-        this.testAddition();
+        let testMethods = this.getAllTestMethods();
+        testMethods.forEach(m => {
+            let method = Reflect.get(this, m);
+            Reflect.apply(method, this, []);
+        });
     }
 }
 
