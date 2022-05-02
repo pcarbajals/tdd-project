4ea5d43751dc91183381424d386b7d7c6e5906b5
Pablo Carbajal <pcarbajals@gmail.com>
2022-05-02 16:05:38 -0400

    Adds test class and test methods

--- js/test_money.js
+++ js/test_money.js
@@ -3,15 +3,32 @@ const Money = require('./money')
 const Portfolio = require('./portfolio')
 
 
-let tenEuros = new Money(10, "EUR");
-let twentyEuros = new Money(20, "EUR");
-assert.deepStrictEqual(tenEuros.times(2), twentyEuros)
+class MoneyTest {
+    testMultiplication() {
+        let tenEuros = new Money(10, "EUR");
+        let twentyEuros = new Money(20, "EUR");
+        assert.deepStrictEqual(tenEuros.times(2), twentyEuros)
+    }
 
-let money = new Money(4002, "KRW");
-assert.deepStrictEqual(money.divide(4), new Money(1000.5, "KRW"))
+    testDivision() {
+        let money = new Money(4002, "KRW");
+        assert.deepStrictEqual(money.divide(4), new Money(1000.5, "KRW"))
+    }
 
-let fiveDollars = new Money(5, "USD");
-let tenDollars = new Money(10, "USD");
-let portfolio = new Portfolio();
-portfolio.add(fiveDollars, tenDollars)
-assert.deepStrictEqual(portfolio.evaluate("USD"), new Money(15, "USD"))
+    testAddition() {
+        let fiveDollars = new Money(5, "USD");
+        let tenDollars = new Money(10, "USD");
+        let portfolio = new Portfolio();
+        portfolio.add(fiveDollars, tenDollars)
+        assert.deepStrictEqual(portfolio.evaluate("USD"), new Money(15, "USD"))
+    }
+
+    runAllTest() {
+        this.testMultiplication();
+        this.testDivision();
+        this.testAddition();
+    }
+}
+
+new MoneyTest().runAllTest();
+
